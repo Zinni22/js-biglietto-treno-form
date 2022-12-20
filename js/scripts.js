6b9f472cbb007fae8bfeece9decb22f62e04bc4c
Zineb Atlassi <atlassizineb@gmail.com>
2022-12-20 17:36:13 +0100

    Milestone 1 terminato

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,13 @@ const kmInput = document.getElementById("km-input");
 const ageSelection = document.getElementById("age-selection");
 const submitButton = document.getElementById("submit-button");
 const deleteButton = document.getElementById("delete-button");
+const passengerName = document.getElementById('passenger-name');
+const promotion = document.getElementById('promotion');
+const carrozza = document.getElementById('carrozza');
+const cpCode = document.getElementById('cp-code');
+const price = document.getElementById('price');
+
+const constRandom = Math.random();
 
 
 submitButton.addEventListener('click',
@@ -30,27 +37,31 @@ submitButton.addEventListener('click',
 
         // condizioni prezzo biglietto
         let ticketPrice = (kmInput.value * 0.21);
-        console.log('il prezzo del biglietto intero è', ticketPrice);
 
         if (ageSelection.value == 'Minorenne'){
             ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
             alert('il tuo biglietto è ridotto del 20%');
+            promotion.innerHTML = 'Biglietto Ridotto Minorenni';
             console.log('il prezzo del biglietto sotto ai 18 è', ticketPrice);
         }
         else if (ageSelection.value == 'Over65'){
             ticketPrice = ticketPrice - ((ticketPrice * 40) / 100);
             alert('il tuo biglietto è ridotto del 40%');
+            promotion.innerHTML = 'Biglietto Silver';
             console.log('il prezzo del biglietto sopra ai 65 è', ticketPrice);
         }
         else{
-            alert('il prezzo è intero');
             console.log('il prezzo del biglietto intero è', ticketPrice);
+            promotion.innerHTML = 'Biglietto Standard';
         }
 
-        alert('Prezzo finale: €' + ticketPrice.toFixed(2))
+        // output
+        passengerName.innerHTML = nameInput.value;
+        carrozza.innerHTML = (constRandom * 10).toFixed(0);
+        cpCode.innerHTML = (constRandom * 100000).toFixed(0);
+        price.innerHTML = '€ ' + ticketPrice.toFixed(2);
 
     }
 )
-
 
 

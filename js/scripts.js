43a678f8a065396aedc08cdfbceff4f8fab8f344
Zineb Atlassi <atlassizineb@gmail.com>
2022-12-20 17:02:32 +0100

    Sezione input funzionante

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 /*
 PRIMA PARTE
-1. Chiedere numero di km da percorrere
-2. Chiedere l'età del passeggero utilizzando bottoni di option con 3 possibilità
-3. imposto una varianta secondo la quale 1km equivale a 0.21€
+1. Creo costanti per nome, km, età e bottoni
+2. Creo una funzione al click sul bottone generale
+3. console.log all'interno della funzione per verificare i valori inseriti dall'utente
 4. condizionale: 
  - se sei più piccolo di 18 hai uno sconto del 20%
  - se invece sei più grande di 65 hai uno sconto del 40%
@@ -11,35 +11,46 @@ PRIMA PARTE
 */
 
 
-
-// 1a. chiedo numero km
-let chilometri = prompt('Quanti chilometri devi percorrere?');
-console.log('chilometri da percorrere', chilometri);
-
-// 2a. chiedo numero km
-let age = prompt('Quanti anni hai?');
-console.log('gli anni del passeggero sono', age);
-
-// 3a. imposto prezzo biglietto normale poi gli sconti
-let ticketPrice = (chilometri * 0.21);
-console.log('il prezzo del biglietto intero è', ticketPrice);
+const nameInput = document.getElementById("name-input");
+const kmInput = document.getElementById("km-input");
+const ageSelection = document.getElementById("age-selection");
+const submitButton = document.getElementById("submit-button");
+const deleteButton = document.getElementById("delete-button");
 
 
-// 4a. condizioni
-if (age < 18){
-    ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
-    alert('il tuo biglietto è ridotto del 20%');
-    console.log('il prezzo del biglietto sotto ai 18 è', ticketPrice);
-}
-else if (age > 65){
-    ticketPrice = ticketPrice - ((ticketPrice * 40) / 100);
-    alert('il tuo biglietto è ridotto del 40%');
-    console.log('il prezzo del biglietto sopra ai 65 è', ticketPrice);
-}
-else{
-    alert('il prezzo è intero');
-    console.log('il prezzo del biglietto intero è', ticketPrice);
-}
+submitButton.addEventListener('click',
+    function(){
 
-// 5a. output prezzo finale
-alert('Prezzo finale: €' + ticketPrice.toFixed(2))
+        console.log('Il nome del passeggero è', nameInput.value);
+
+        console.log('I km che deve percorrere sono', kmInput.value);
+
+        console.log("La fascia d'età del passeggero è", ageSelection.value);
+
+
+        // condizioni prezzo biglietto
+        let ticketPrice = (kmInput.value * 0.21);
+        console.log('il prezzo del biglietto intero è', ticketPrice);
+
+        if (ageSelection.value == 'Minorenne'){
+            ticketPrice = ticketPrice - ((ticketPrice * 20) / 100);
+            alert('il tuo biglietto è ridotto del 20%');
+            console.log('il prezzo del biglietto sotto ai 18 è', ticketPrice);
+        }
+        else if (ageSelection.value == 'Over65'){
+            ticketPrice = ticketPrice - ((ticketPrice * 40) / 100);
+            alert('il tuo biglietto è ridotto del 40%');
+            console.log('il prezzo del biglietto sopra ai 65 è', ticketPrice);
+        }
+        else{
+            alert('il prezzo è intero');
+            console.log('il prezzo del biglietto intero è', ticketPrice);
+        }
+
+        alert('Prezzo finale: €' + ticketPrice.toFixed(2))
+
+    }
+)
+
+
+
